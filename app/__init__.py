7f0ddd67978cbae9b651d5ebdc1a8599b696f077
Lincan Li <email@lincan.li>
2016-08-18 15:03:02 +0800

    加入 cors 支持

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from config import config
+from flask_cors import CORS, cross_origin
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
 
     app.config.from_object(config[config_name])
     app.debug = True
